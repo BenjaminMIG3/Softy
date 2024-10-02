8ef2710b06010d89c89c760e962b335dbc10f0de
BenjaminMIG3 <122969282+BenjaminMIG3@users.noreply.github.com>
2024-10-02 23:19:13 +0200

    maj 02/10/2024

--- softy/src/main/java/com/softy/softy/Controller/GetUrls.java
+++ softy/src/main/java/com/softy/softy/Controller/GetUrls.java
@@ -11,14 +11,8 @@ public class GetUrls {
 
     @GetMapping("/")
     public String index(HttpSession session, Model model) {
-        // Vérifie si la session est null ou si l'utilisateur n'est pas connecté
-        if (session == null || session.getAttribute("user") == null) {
-            System.out.println("Aucun utilisateur connecté. Redirection vers la page de connexion.");
-            // return "redirect:/login"; // Redirection vers la page de connexion
-        }
         // Récupère l'utilisateur de la session
         Utilisateur user = (Utilisateur) session.getAttribute("user");
-        System.out.println("Dans la session : " + user); // Affiche les détails de l'utilisateur
         // Optionnel : ajouter l'utilisateur au modèle pour la vue
         model.addAttribute("user", user);
         return "index"; // Renvoie le nom de la vue
@@ -33,4 +27,10 @@ public class GetUrls {
     public String signin(){
         return "signin";
     }
+
+    @GetMapping("/caisse/menu")
+    public String caisseMenu(){
+        return "index_caisse";
+    }
+
 }
